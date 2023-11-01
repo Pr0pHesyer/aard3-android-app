860fc8d41670855e1397194c8dd1f065bc15c4ee
Pr0pHesyer <74277431+Pr0pHesyer@users.noreply.github.com>
2023-11-01 21:31:10 +0800

    First Commit

---
+++ src/main/java/com/pr0phesyer/aard3/ui/history/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.pr0phesyer.aard3.ui.history
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
